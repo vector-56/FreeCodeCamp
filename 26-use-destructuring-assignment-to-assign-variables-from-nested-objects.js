5ede518460b92cf8d2c04530509788c0e5313b85
Sergey Danusevich <vector-56@yandex.ru>
2021-12-11 06:51:55 +0300

    add works 26 & 27

---
+++ 26-use-destructuring-assignment-to-assign-variables-from-nested-objects.js
@@ -0,0 +1,13 @@
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 }
+};
+
+// Only change code below this line
+  
+const { yesterday : {low: lowYesterday, high: highYesterday} } = LOCAL_FORECAST
+const { today : {low: lowToday, high: highToday} } = LOCAL_FORECAST
+const { tomorrow : {low: lowTomorrow, high: highTomorrow} } = LOCAL_FORECAST
+
+// Only change code above this line
